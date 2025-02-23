39d9f7c2f1ae3fa4e37adc1c055c23b026dbfd6d
eb546 <eb546@kent.ac.uk>
2025-02-23 20:16:58 +0000

    fin

--- eb546_EENG5600_Assignment_1.c
+++ eb546_EENG5600_Assignment_1.c
@@ -13,7 +13,23 @@ int readin(void)
     note that this function should return the number of
     masses read in from the file */
 
+    int n_masses;
+    printf("Enter the number of weights: ");
+    scanf("%d", &n_masses);
 
+    if (n_masses > MAX)
+        {
+            printf("Error! Number of wights exceeded the maximum limit.\n");
+            return 0;
+        }
+
+    for (int i = 0; i < n_masses; i++)
+        {
+            printf("Enter data of weight for each coordinates %d (x_coordinate, y_coordinate, mass): ", i +1);
+            scanf("%f %f %f", &masses[i].x, &masses[i].y, &masses[i].mass);
+        }
+
+    return n_masses;
 }
 
 void computecg(int n_masses)
@@ -22,16 +38,28 @@ void computecg(int n_masses)
     /* Write this function to compute the C of G
     and print the result */
 
-    float x, y, mass;
+    float sum_of_xmass = 0.0;
+    float sum_of_ymass = 0.0;
+    float sum_of_mass = 0.0;
 
-    printf("Enter a value for x-coordinate: ");
-    scanf("%s", x);
+    for(int i = 0; i < n_masses; i++)
+        {
+            sum_of_xmass += masses[i].x * masses[i].mass;
+            sum_of_ymass += masses[i].y * masses[i].mass;
+            sum_of_mass += masses[i].mass;
+        }
 
-    printf("Enter a value for y-coordinate: ");
-    scanf("%s", y);
+    if (sum_of_mass == 0)
+        {
+            printf("Error! Result is 0. Cannot compute center of gravity!.\n");
+            return;
+        }
+    
+    float cg_x = sum_of_xmass / sum_of_mass;
+    float cg_y = sum_of_ymass / sum_of_mass;
 
-    printf("Sum of x is: ");
-
+    printf("COG coordinates are: x = %.2f y = %.2f\n", cg_x, cg_y);
+    
 }
 
 int main(void)
